1b917ae37367984a0d7021064e73cd39e657f12e
James <jamesbrad1309@gmail.com>
2016-06-06 15:50:56 +0700

    Basic set up for about us section

---
+++ front-page.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+
+<section class="welcome">
+    <div class="container">
+        <div class="text_content">
+            <h1 class="welcome_text">asdfasdfasdfas</h1>
+            <span class="sub_text">asdfasdfasdfasdfasdfasfd</span>
+
+            <div class="control">
+                <a href="#" class="btn btn-red">Learn More</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="about-us">
+    <div class="container">
+        <div class="icon">
+
+        </div>
+
+        <div class="text-content">
+            <div class="title">
+                Lorem ipsum dolor sit amet.
+            </div>
+            <div class="subtitle">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis beatae, et nemo nostrum odio eveniet atque eius. Aliquam laudantium, hic laboriosam quas voluptatum ipsa! Natus quisquam atque fugiat sed, explicabo!
+            </div>
+        </div>
+
+        <div class="row info">
+            <div class="col-md-6">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet delectus, laborum ipsa magnam. Quos quibusdam possimus rerum explicabo dolorem dolores, hic nam nostrum animi voluptatem perferendis eveniet magni, cupiditate ea!  
+            </div>
+
+            <div class="col-md-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda, harum maxime error impedit quam minus. Cupiditate architecto ipsa esse cumque commodi, sapiente accusamus mollitia quis. Ducimus ipsa ipsum quae porro, odio excepturi, ad totam, vero laudantium illo labore. Pariatur dolorem tempore, minus iure asperiores aperiam, rerum fugit molestiae commodi.
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
