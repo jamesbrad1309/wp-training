9e48529e5e060173217852a680d57bed88698e98
james <jamesbrad1309@gmail.com>
2016-06-07 04:45:56 +0700

    html footer completed

--- footer.php
+++ footer.php
@@ -4,7 +4,74 @@
 	<div class="up">
 	</div>
 
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="title">
+					About us
+				</div>
+				<span class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, eaque officiis ullam, ratione vel inventore labore pariatur iste veniam blanditiis eveniet, doloribus mollitia porro, possimus cumque aliquam fugiat natus sit.</span>
+			</div>
+			<div class="col-md-3">
+				<div class="title">
+					Service
+				</div>
+				<span class="content">
+					<ul>
+						<li>somethign</li>
+						<li>something2</li>
+						<li>something3</li>
+					</ul>
+				</span>
+			</div>
+			<div class="col-md-3">
+				<div class="title">
+					Bussiness Hour
+				</div>
+				<span class="content">
+					<ul>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, neque.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, nemo.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+					</ul>
+				</span>
+			</div>
+			<div class="col-md-3">
+				<div class="title">
+					Keep in touch
+				</div>
+				<div class="content">
+					<div class="form-container">
+						<form action="#">
+							<input type="text" placeholder="something">
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+
+					<div class="sns">
+						<div class="ggp_footer"><i class="fa fa-google-plus" aria-hidden="true"></i></div>
+						<div class="fb_footer"><i class="fa fa-facebook" aria-hidden="true"></i></div>
+						<div class="tw_footer"><i class="fa fa-twitter" aria-hidden="true"></i></div>
+						<div class="li_footer"><i class="fa fa-linkedin" aria-hidden="true"></i></div>
+					</div>
+
+					<div class="contact-detail">
+						<div class="detail">
+							<i class="fa fa-map-marker" aria-hidden="true"></i><span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, maxime.</span>
+						</div>
+
+						<div class="detail">
+							<i class="fa fa-envelope" aria-hidden="true"></i><span> Lorem ipsum dolor sit amet.</span>
+						</div>
+						
+						<div class="detail">
+							<i class="fa fa-phone" aria-hidden="true"></i><span> 32165498</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
